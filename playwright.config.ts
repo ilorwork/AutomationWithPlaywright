ed9b3a1efa946b846fd3b585b29326762082dfeb
ilorwork <ilorwork64@gmail.com>
2023-10-26 01:22:44 +0300

    Add some commented optional configs

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,6 +21,13 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+
+  // path to the global setup files.
+  // globalSetup: require.resolve("./global-setup"),
+
+  // path to the global teardown files.
+  // globalTeardown: require.resolve("./global-teardown"),
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
