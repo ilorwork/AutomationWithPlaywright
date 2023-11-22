d778cebc30a7d1ad83089a4592fb0e251f14a514
Ilors Digital <ilors@digital.gov.il>
2023-11-22 11:54:29 +0200

    Execute the auth solution using test fixture

---
+++ fixtures/pfxLoginFixture.ts
@@ -0,0 +1,54 @@
+// fixtures.ts
+import ks from "node-key-sender";
+import { Page, test as base } from "@playwright/test";
+
+// export const test = base.extend({
+//   page: async ({ page }, use) => {
+//     await use(page);
+
+//     try {
+//       await page.goto("", { timeout: 2000 });
+//     } catch (error) {
+//       ks.setOption("globalDelayPressMillisec", 1000);
+//       // Do not touch the keyboard while this code run!!
+//       await ks.sendKey("down");
+//       await ks.sendKey("enter");
+//     }
+//   },
+// });
+
+export const test = base.extend<{ page: Page }>({
+  // the <{ page: Page }> is not mandatory, but it looks like I should add it.
+  page: async ({ page }, use) => {
+    try {
+      await page.goto("", { timeout: 2000 });
+    } catch (error) {
+      ks.setOption("globalDelayPressMillisec", 1000);
+      // Do not touch the keyboard while this code run!!
+      await ks.sendKey("down");
+      await ks.sendKey("enter");
+    }
+    await use(page); // looks like the this use call need to be at the end of this code.
+    // i don't understand why it behave like that or what it does and where it came from.
+  },
+});
+
+/* 
+the key to make it work was to call `await use(page)` at the end of the fixture setup.
+
+The reason for this is that `use` is actually a function that Playwright injects and passes to the fixture. This `use` function will execute the test body.
+
+So by calling `await use(page)` at the end, it essentially chains things to:
+
+1. Run the fixture setup 
+2. Pass control to the test body via `use()`
+3. Then the test body executes with the fixture applied
+
+Without calling `use()` the control would never get passed back to the test body.
+
+So while it may seem unusual to call a function that is passed in, that is how Playwright handles the execution flow under the hood.
+
+In addition, the generic type syntax is not mandatory, but can help get auto-completions during development.
+
+Understanding these fixtures and execution flow is very valuable for unlocking the power of Playwright.
+*/
---
+++ tests/smartCardAuth/authUsingKeySender.spec.ts
@@ -0,0 +1,69 @@
+import ks from "node-key-sender";
+import NotImplementedError from "../../helpers/errors";
+import { expect } from "@playwright/test";
+import { test } from "../../fixtures/pfxLoginFixture";
+
+test("test1", async ({ page }) => {
+  await page.locator("#edit-name").fill("fdsafdsa");
+  await page.locator("#edit-pass").fill("54235423");
+  await page.locator("#edit-submit").click();
+
+  //   await expect(page.locator(".messages.error")).toBeVisible();
+  await expect(page.locator(".messages.error")).toContainText(
+    "שם המשתמש והסיסמא אינם מוכרים"
+  );
+
+  await page.waitForTimeout(2000);
+});
+
+test.skip("handle authentication using node-key-sender", async ({ page }) => {
+  try {
+    await page.goto("", { timeout: 2000 });
+  } catch (error) {
+    ks.setOption("globalDelayPressMillisec", 1000);
+    // Do not touch the keyboard while this code run!!
+    await ks.sendKey("down");
+    await ks.sendKey("enter");
+  }
+
+  // await ks.sendKey("enter");
+  // await ks.sendKey("0");
+  // await ks.sendKey("0");
+  // await ks.sendKey("0");
+  // await ks.sendKey("0");
+  // await ks.sendKey("enter");
+
+  // await ks.sendKey(["0", "0", "0", "0"]);
+
+  //   await page.goto(""); // should use credentials
+
+  await page.locator("#edit-name").fill("fdsafdsa");
+  await page.locator("#edit-pass").fill("54235423");
+  await page.locator("#edit-submit").click();
+
+  //   await expect(page.locator(".messages.error")).toBeVisible();
+  await expect(page.locator(".messages.error")).toContainText(
+    "שם המשתמש והסיסמא אינם מוכרים"
+  );
+
+  await page.waitForTimeout(2000);
+
+  // await page.pause()
+});
+
+// test("handle", async ({}) => {
+//   //   await page.goto(""); // should use credentials
+
+//   await page.locator("#edit-name").fill("fdsafdsa");
+//   await page.locator("#edit-pass").fill("54235423");
+//   await page.locator("#edit-submit").click();
+
+//   //   await expect(page.locator(".messages.error")).toBeVisible();
+//   await expect(page.locator(".messages.error")).toContainText(
+//     "שם המשתמש והסיסמא אינם מוכרים"
+//   );
+
+//   await page.waitForTimeout(2000);
+
+//   // await page.pause()
+// });
