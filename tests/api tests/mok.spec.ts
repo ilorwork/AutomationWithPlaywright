6765a84dff28dc6ecc0051d69d08f4717a6582ac
ilorwork <ilorwork64@gmail.com>
2023-11-24 16:19:44 +0200

    Add api request modification test, and orgnize api tests

--- tests/api tests/mok.spec.ts
+++ tests/api tests/mok.spec.ts
@@ -5,7 +5,10 @@ test.beforeEach(async ({ page }) => await page.goto(""));
 // the first one by blocking all images requests,
 // and the sec one by replacing/moking the icon with a simple js object- which isn't an image of course.
 
+// Note: Enabling routing disables http cache.
+
 test("block all images requests", async ({ page }) => {
+  //   await page.route("**/*.{png,jpg,jpeg}", (route) => route.abort()); // another option
   await page.route(/(png|jpeg)$/, (route) => route.abort());
 
   await expect(page).toHaveTitle(/MeetMusic/);
