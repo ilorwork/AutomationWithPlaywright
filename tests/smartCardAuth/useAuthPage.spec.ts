539499610566a582a7cda7e185032e7e0b34ca63
Ilors Digital <ilors@digital.gov.il>
2023-12-11 15:44:11 +0200

    Add link to google chrome reg configuration docs

--- tests/smartCardAuth/useAuthPage.spec.ts
+++ tests/smartCardAuth/useAuthPage.spec.ts
@@ -6,6 +6,8 @@ test("Use AuthPage to authenticate", async ({ page }) => {
 
   // await page.goto("");
 
+  // Link to google chrome reg configuration docs
+  // https://support.google.com/chrome/a/answer/9131254?hl=en#zippy=%2Cconfigure-settings-on-windows-computers
   await authPage.goToAuthURL(
     process.env.PFX_USER1 as string,
     process.env.BASE_URL as string
