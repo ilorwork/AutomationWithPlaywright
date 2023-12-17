9a768af95babdd01543cd1d130ba80381c3d784b
ilorsDigital <ilors@digital.gov.il>
2023-12-17 15:29:44 +0200

    Remove useless code and rename test

---
+++ tests/smartCardAuth/autoSelectRegPolicyKey.spec.ts
@@ -0,0 +1,10 @@
+import test from "@playwright/test";
+import UseRegistryKey from "../../helpers/reg/regConverted";
+
+test("Use Auto select registry key to authenticate", async ({ page }) => {
+  await UseRegistryKey.signUserToRegAndNav(
+    process.env.PFX_USER1 as string,
+    process.env.BASE_URL as string,
+    page
+  );
+});
