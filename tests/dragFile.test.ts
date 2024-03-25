afd71043dd733547d9eee8be8ce2ff446087d6d4
Ilors Digital <ilors@digital.gov.il>
2024-03-25 16:57:22 +0200

    Add some explenation for "drag and drop" test

--- tests/dragFile.test.ts
+++ tests/dragFile.test.ts
@@ -1,11 +1,15 @@
 import test from "@playwright/test";
 import { readFileSync } from "fs";
 
+// This exapmle test simulates image drag-and-drop from pc to the drop-zone of www.dragdrop.com test site.
+// In order for this test to work you should create an image file called "image-1.png",
+// and place it at the project's root directory.
+
 test("drag and drop file", async ({ page }) => {
   // Navigate to the page with the drop zone
   await page.goto("https://www.dragdrop.com/test/");
 
-  // Read your file into a buffer.
+  // Read your file into a buffer
   const buffer = readFileSync("./image-1.png");
 
   // Create the DataTransfer and File
@@ -19,9 +23,10 @@ test("drag and drop file", async ({ page }) => {
     return dt;
   }, buffer);
 
-  // Now dispatch
+  // Now dispatch the "drop" event
   await page.dispatchEvent("#demo-upload", "drop", { dataTransfer });
 
+  // Let the tester see the result
   await page.waitForTimeout(4000);
 
   // Clear
